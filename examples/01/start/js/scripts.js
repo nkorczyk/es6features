cc8be0ea9b23706b02598290d0c3f6933a7db1d7
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-11-16 11:01:30 +0100

    01 - finished

--- examples/01/start/js/scripts.js
+++ examples/01/start/js/scripts.js
@@ -1,5 +1,17 @@
-(function() {
+(function () {
 
+    var firstName = "Jan";
+    console.log(firstName + " " + getlastName());
 
+    function getlastName() {
+        return "Kowalski";
+    }
 
+    if (false) {
+        var lastName = "Kowalski";
+    }
+
+    console.log(lastName);
 })();
+
+var firstName = "Jan";
