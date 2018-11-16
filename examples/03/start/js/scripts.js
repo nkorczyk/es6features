69d2cc7f79997a6ae1708b794eaccd14d62e4d4c
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-11-16 11:46:20 +0100

    03 - finished

--- examples/03/start/js/scripts.js
+++ examples/03/start/js/scripts.js
@@ -1,5 +1,18 @@
-(function() {
+(function () {
 
+    const API_URL = "http://api.mojastrona.pl";
 
+    const getName = function () {
+        return "Kowalski";
+    }
+
+    if (true) {
+        let name = "Jan " + getName();
+        console.log(name);
+    }
+    
+    for (let i = 0; i < 10; i++) {
+        console.log(i);
+    }
 
 })();
