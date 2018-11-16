7884200c0f8a0ce900e28bfda384e56f45297feb
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-11-16 12:10:01 +0100

    04 - finished

--- examples/04/start/js/scripts.js
+++ examples/04/start/js/scripts.js
@@ -1,2 +1,45 @@
 let firstName = "Jan",
     lastName = "Kowalski";
+
+let person = {
+    firstName,
+    lastName, 
+    getFullName() {
+        return this.firstName + " " + this.lastName;
+    }
+};
+
+console.log(person);
+console.log(person.getFullName());
+
+let object = {
+    toString() {
+        return "Super";
+    }
+};
+
+console.log("" + object);
+
+let fnName = "getFullName";
+
+let person2 = {
+    firstName,
+    lastName,
+};
+
+person2[fnName] = function () {
+    return this.firstName + " " + this.lastName;
+}
+
+console.log(person2.getFullName());
+
+let person3 = {
+    firstName,
+    lastName,
+    [fnName + 1]: "Witaj",
+    [fnName]() {
+        return this.firstName + " " + this.lastName;
+    }
+}
+;
+console.log(person3.getFullName());
