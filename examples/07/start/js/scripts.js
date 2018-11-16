b7c6bc203d4057f7276d1cb8202150f26999d0fc
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-11-16 19:21:22 +0100

    07 - finished

--- examples/07/start/js/scripts.js
+++ examples/07/start/js/scripts.js
@@ -1,7 +1,42 @@
+// let person = {
+//     firstName: "Jan",
+//     lastName: "Kowalski",
+//     sayHello: () => {
+//         return this.firstName + " " + this.lastName;
+//     }
+// };
+
 let person = {
     firstName: "Jan",
     lastName: "Kowalski",
-    sayHello: () => {
-        return this.firstName + " " + this.lastName;
+    // sayHello: function () {
+    //     setTimeout(function () {
+    //         console.log(this.firstName + " " + this.lastName);
+    //     }.bind(this), 2000);
+    // }
+    // sayHello() {
+    //     setTimeout(() => {
+    //         console.log(this.firstName + " " + this.lastName);
+    //     }, 2000);
+    // }
+    sayHello() {
+        console.log(this.firstName + " " + this.lastName);
     }
 };
+
+person.sayHello.call({
+    firstName: "Anna",
+    lastName: "Nowak"
+});
+
+document.querySelector("#button-07").onclick = function(event) {
+    console.log(event.target === this);
+};
+
+let Person = (firstName) => {
+    this.firstName = firstName;
+};
+
+console.log(Person.prototype);
+
+let person1 = new Person("Anna");
