fc8e61290f2f3838f3e304b432139d13f403e35c
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-11-17 23:25:00 +0100

    18 - finished

--- examples/18/start/js/scripts.js
+++ examples/18/start/js/scripts.js
@@ -6,4 +6,24 @@ let person = {
 
 let { firstName, lastName, age } = person;
 
-let info = "Imię: " + firstName + ", nazwisko: " + lastName + ", wiek: " + age + " lat.";
+// let info = "Imię: " + firstName + ", nazwisko: " + lastName + ", wiek: " + age + " lat.";
+
+// let info = `Imię: ${true ? "Piotr" : "Paweł"}, nazwisko: ${lastName}, wiek: ${age} lat.`;
+let info = `Imię: ${firstName}, nazwisko: ${lastName}, wiek: ${age} lat.`;
+
+console.log(info);
+
+let buttonText = "Wcisnij mnie";
+
+// let template = "\
+// <button class='btn'>\
+//     <span>" + buttonText + "</span>\
+// </button>";
+
+let template = `
+<button class='btn'>
+    <span>${buttonText}</span>
+</button>
+`;
+
+console.log(template);
