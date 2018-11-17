3107c4be0132eaca32f16ba49c00027e4bf23ef6
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-11-17 16:21:39 +0100

    10 - finished

--- examples/10/start/js/scripts.js
+++ examples/10/start/js/scripts.js
@@ -1,5 +1,19 @@
-function getName() {
+let getName = function () {
 
+    throw new Error("Wystąpił błąd!");
+    
     return "Jan";
+    
+};
 
-}
+let fn = getName;
+
+let person = {
+    getName: () => {
+        throw new Error("Wystąpił błąd!");
+    }
+};
+
+let newFn = getName.bind(null);
+
+let getPersonName = new Function("", "return 'Jan';");
