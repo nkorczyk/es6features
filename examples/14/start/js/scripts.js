b94583a148af20f93fd1020d8cfee22e6045669d
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-11-17 19:05:01 +0100

    14 - finished

--- examples/14/start/js/scripts.js
+++ examples/14/start/js/scripts.js
@@ -1,1 +1,23 @@
 let numbers = [10, 20, 30, 40, 50];
+
+// let first = numbers[0],
+//     second = numbers[1];
+
+// let [first, second] = numbers;
+// let [first, second, , fourth] = numbers;
+
+let first, second, fourth;
+
+// let [first, second, , fourth] = numbers || [];
+[first, second, , fourth] = numbers || [];
+
+console.log(first, second, fourth);
+
+let a = 1,
+b = 2;
+
+console.log(a, b);
+
+[b, a] = [a, b];
+
+console.log(a, b);
