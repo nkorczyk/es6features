1cef7ade6740392179170daf206e114ac283bc5c
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-11-17 16:57:52 +0100

    12 - finished

--- examples/12/start/js/scripts.js
+++ examples/12/start/js/scripts.js
@@ -1,1 +1,35 @@
 let numbers = [12, 3, 9, 22, 11, 6];
+
+// let max = Math.max(12, 3, 9, 22, 11, 6);
+console.log(Math.max.apply(Math, numbers));
+
+let max = Math.max(...numbers);
+
+let someString = "Norbert";
+
+console.log(...someString);
+
+console.log(Math.max(100, ...numbers));
+
+let numbers2 = [2, 33, 10].concat(numbers);
+
+let numbers3 = [2, 33, 10, ...numbers, 1, 75];
+
+console.log([...numbers3, ...numbers2, 100]);
+
+let s = [...numbers2];
+
+console.log([...numbers3, ...numbers2, 100, ..."Piotr"]);
+
+function strToArray(string = "") {
+    // return string.split("");
+    return [...string];
+}
+
+console.log(strToArray("Norbert"));
+
+function reverse(str = "") {
+    return [...str].reverse().join("");
+}
+
+console.log(reverse("Norbert"));
