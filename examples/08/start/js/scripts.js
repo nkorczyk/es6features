76ebf9a9e10c68401cf2ecb0de1834c5c84c09f5
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-11-17 16:04:26 +0100

    08 - finished

--- examples/08/start/js/scripts.js
+++ examples/08/start/js/scripts.js
@@ -1,5 +1,21 @@
-function multiply(number, multiplyBy) {
+function multiply(number, multiplyBy = 2) {
+
+    // multiplyBy = multiplyBy || 2;
+    // multiplyBy = multiplyBy === undefined ? 2 : multiplyBy;
 
     return number * multiplyBy;
 
+}
+
+function getCountryCode(country, code = country.toUpperCase().slice(0, 3)) {
+    console.log("Wykonuje f-cje getCountryCode");
+
+    return {
+        country,
+        code
+    };
+}
+
+function getCountryInfo(countryInfo = getCountryCode("Polska")) {
+    return "Państwo to " + countryInfo.country + ", a jego kod to " + countryInfo.code;
 }
