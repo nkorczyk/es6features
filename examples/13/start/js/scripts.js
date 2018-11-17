3134314bb3d03c40f8bd24975bf19501035e43da
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-11-17 18:48:03 +0100

    13 - finished

--- examples/13/start/js/scripts.js
+++ examples/13/start/js/scripts.js
@@ -3,3 +3,25 @@ let person = {
     lastName: "Kowalski",
     age: 49
 };
+
+// let firstName = person.firstName,
+//     lastName = person.lastName,
+//     age = person.age;
+
+// console.log(firstName, lastName, age);
+
+function getData() {
+    return null;
+}
+
+// let { firstName, lastName, age } = getData() || {};
+// let { firstName: fName, lastName, age } = getData() || {};
+//let { firstName, lastName, age, job } = person;
+
+
+let firstName, lastName, age;
+
+({ firstName: fName, lastName, age } = person);
+
+// console.log(firstName, lastName, age);
+console.log(fName, lastName, age);
