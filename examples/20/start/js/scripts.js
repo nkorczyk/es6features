7ab42d494953a9ab8f0e42d7880c54acd6a0854c
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-11-18 00:09:47 +0100

    20 - finished

--- examples/20/start/js/scripts.js
+++ examples/20/start/js/scripts.js
@@ -1,2 +1,31 @@
 const URL = "https://mojastrona.pl";
 const filePath = "/Users/janek/Desktop/app/index.html";
+
+// function isHTTPS(text) {
+//     return text.indexOf("https://") === 0;
+// }
+
+function isHTTPS(text) {
+    return text.startsWith("https");
+}
+
+// function hasExt(path, ext) {
+//     return (new RegExp("\." + ext + "$")).test(path);
+// }
+
+function hasExt(path, ext) {
+    return path.endsWith(`.${ext}`);
+}
+
+// function includes(text, substring) {
+//     return text.indexOf(substring) !== -1;
+// }
+
+function includes(text, substring) {
+    return text.includes(substring);
+}
+
+console.log(hasExt(filePath, "html"));
+console.log(isHTTPS(URL));
+console.log(includes("Ala ma kota", "ma"));
+console.log("=".repeat(10));
