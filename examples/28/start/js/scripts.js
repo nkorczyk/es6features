7315fb01d1605d3765ec0533a4f9c919701dc9fa
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-11-18 14:36:12 +0100

    28 - finished

--- examples/28/start/js/scripts.js
+++ examples/28/start/js/scripts.js
@@ -1,8 +1,22 @@
-const hidden = Symbol("hidden");
+// const hidden = Symbol("hidden");
+
+// (function() {
+//     const hidden = Symbol.for("hidden");
+// })();
+
+const hidden = Symbol.for("hidden");
 
 let person = {
     [hidden]: "123jkasdKhasdf$901-123",
     getSecret() {
-        return this[hidden];
+        return this[Symbol.for("hidden")];
     }
 };
+
+console.log(Symbol.keyFor(hidden));
+
+console.log(Object.getOwnPropertyNames(person));
+
+console.log(Object.getOwnPropertySymbols(person));
+
+let abc = Object.getOwnPropertySymbols(person);
