56a28cccccd0385374d3da8028698713345f7b0b
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-11-18 12:17:25 +0100

    26 - finished

--- examples/26/start/js/scripts.js
+++ examples/26/start/js/scripts.js
@@ -1,23 +1,64 @@
-function Person(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-}
-
-Person.prototype.sayHello = function() {
-    return this.firstName + " " + this.lastName;
-};
-
-// class Person {
-
-//     constructor(firstName, lastName) {
-//         this.firstName = firstName;
-//         this.lastName = lastName;
-//     }
-
-//     sayHello() {
-//         return `${this.firstName} ${this.lastName}`;
-//     }
-
+// function Person(firstName, lastName) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
 // }
 
-let person1 = new Person("Jan", "Kowalski");
+// Object.defineProperty(Person.prototype, "sayHello", {
+//     enumerable: false,
+//     value: function() {
+//         return this.firstName + " " + this.lastName;
+//     }
+// });
+
+// Person.prototype.sayHello = function() {
+//     return this.firstName + " " + this.lastName;
+// };
+
+class Person {
+
+    constructor(firstName, lastName) {
+        if (new.target === Person) {
+            throw new Error("Klasy Person nie można używać bezpośrednio");
+        }
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    sayHello() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
+class Employee extends Person {
+
+    constructor(firstName, lastName, position) {
+        super(firstName, lastName);
+        this.position = position;
+    }
+
+    sayHello() {
+        return `Nazywam się ${super.sayHello()} i pracuję jako ${this.position}.`;
+    }
+}
+
+// let person1 = new Person("Jan", "Kowalski");
+
+let employee1 = new Employee("Jan", "Kowalski", "Programista");
+
+console.log(employee1.sayHello());
+
+// for (let key in person1) {
+//     console.log(key);
+// }
+
+function createInstance(fromClass, ...args) {
+    return new fromClass(...args);
+}
+
+let person2 = createInstance(class {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}, "Anna", "Nowak");
