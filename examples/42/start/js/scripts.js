96f3bd03861165c6110839809fee11baa9d1688e
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-11-25 16:41:24 +0100

    42 - finished

--- examples/42/start/js/scripts.js
+++ examples/42/start/js/scripts.js
@@ -8,18 +8,18 @@ function getJSON(url) {
 
     xhr.open("GET", url);
 
-    let p = new Promise(function(resolve, reject) {
+    let p = new Promise(function (resolve, reject) {
 
-        xhr.onload = function() {
-            if(xhr.status === 200) {
+        xhr.onload = function () {
+            if (xhr.status === 200) {
                 resolve(xhr.responseText);
             } else {
-                reject( new Error("Wystąpił błąd") );
+                reject(new Error("Wystąpił błąd"));
             }
         };
 
-        xhr.onerror = function() {
-            reject( new Error("Wystapił błąd") );
+        xhr.onerror = function () {
+            reject(new Error("Wystapił błąd"));
         };
 
     });
@@ -30,8 +30,34 @@ function getJSON(url) {
 
 }
 
-$("#btn-42").onclick = function() {
+function timeout(ms) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(() => reject(new Error(`Czas ${ms}ms został przekroczony`)), ms);
+    });
+}
 
+$("#btn-42").onclick = function () {
 
+    // Promise.all([
+    //     getJSON("http://code.eduweb.pl/kurs-es6/json/?shuffle=1"),
+    //     getJSON("http://code.eduweb.pl/kurs-es6/json/?shuffle=1")
+    // ])
+    // .then(data => {
+    //     $("#pre-42").textContent = `${data[0]}\n\n${"=".repeat(70)}\n\n${data[1]}`;
+    // })
+    // .catch(err => $("#pre-42").textContent = err.message);
 
+    // Promise.race([
+    //     getJSON("http://code.eduweb.pl/kurs-es6/json/?shuffle=1"),
+    //     getJSON("http://code.eduweb.pl/kurs-es6/json/?shuffle=1")
+    // ])
+    // .then(json => $("#pre-42").textContent = json)
+    // .catch(err => $("#pre-42").textContent = err.message);
+
+    Promise.race([
+        getJSON("http://code.eduweb.pl/kurs-es6/json/?shuffle=1"),
+        timeout(100)
+    ])
+    .then(json => $("#pre-42").textContent = json)
+    .catch(err => $("#pre-42").textContent = err.message);
 };
