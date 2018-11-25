c2068993f7220a7ffff0e53dce90a17f1e15e2d3
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-11-25 19:03:26 +0100

    47 - finished

--- examples/47/start/js/scripts.js
+++ examples/47/start/js/scripts.js
@@ -7,3 +7,38 @@ let person2 = {
     firstName: "Anna",
     lastName: "Nowak"
 };
+
+let age = new WeakMap([[person1, 32], [person2, 22]]);
+
+console.log(age);
+
+preson1 = null;
+
+console.log(age);
+
+const Person = (function () {
+    
+    const privateDate = new WeakMap();
+    
+    return class Person {
+        constructor(firstName, lastName) {
+            privateDate.set(this, {
+                firstName,
+                lastName
+            });
+        }
+        
+        sayHello () {
+            let data = privateDate.get(this);
+
+            return `${data.firstName} ${data.lastName}`;
+        }
+    };
+})();
+
+let person3 = new Person("Jan", "Kowalski");
+
+// console.log(person3.firstName); // teraz firstName i lastName są prywatne
+console.log(person3.sayHello());
+
+person3 = null;
